3f797cbc69d83520db8810083ba444870b29c0af
Niklas Decker <niki25.9@web.de>
2021-07-21 19:44:17 +0200

    haufen Zeug erstellt

--- app/src/main/java/me/olisonsturm/blackout/view/LoadingActivity.java
+++ app/src/main/java/me/olisonsturm/blackout/view/LoadingActivity.java
@@ -2,15 +2,35 @@ package me.olisonsturm.blackout.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import me.olisonsturm.blackout.R;
 
 public class LoadingActivity extends AppCompatActivity {
+
+    @BindView(R.id.startImage)
+    ImageView startImage;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
+        ButterKnife.bind(this);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(this, LobbyActivity.class));
+            finish();
+        }, 1000);
     }
 }
---
+++ app/src/main/java/me/olisonsturm/blackout/view/LobbyActivity.java
@@ -0,0 +1,16 @@
+package me.olisonsturm.blackout.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import me.olisonsturm.blackout.R;
+
+public class LobbyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lobby);
+    }
+}
