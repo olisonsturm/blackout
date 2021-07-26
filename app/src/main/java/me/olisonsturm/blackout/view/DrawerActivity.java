fd38e1dd472651b96a846bf2512d93677e3d0d20
Niklas Decker <niki25.9@web.de>
2021-07-26 23:52:20 +0200

    erster Versuch von nem Navigationdrawer

---
+++ app/src/main/java/me/olisonsturm/blackout/view/DrawerActivity.java
@@ -0,0 +1,16 @@
+package me.olisonsturm.blackout.view;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import me.olisonsturm.blackout.R;
+
+public class DrawerActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drawer);
+    }
+
+}
