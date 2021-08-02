7f2c553b7e50129163546eb12a466dddd03911b2
Niklas Decker <niki25.9@web.de>
2021-08-02 16:07:41 +0200

    ernsthafte Veränderungen: DrawerActivity.java und activity_drawer.xml waren unnötig und das musste in die Lobby jetzt hängt es nur noch an der Toolbar LobbyActivity Line 25

--- app/src/main/java/me/olisonsturm/blackout/view/LobbyActivity.java
+++ app/src/main/java/me/olisonsturm/blackout/view/LobbyActivity.java
@@ -7,19 +7,23 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.ViewGroup;
+
 import me.olisonsturm.blackout.R;
 
 public class LobbyActivity extends AppCompatActivity {
     private DrawerLayout drawer;
+            Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lobby);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
         drawer = findViewById(R.id.drawer_layout);
+        toolbar = findViewById(R.id.myToolbar);
+        setSupportActionBar(toolbar);
+
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
