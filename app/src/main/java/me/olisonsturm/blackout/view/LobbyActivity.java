2637bcf33e13211f817174b2a6be40648c5eaf7d
Niklas Decker <niki25.9@web.de>
2021-08-02 20:13:44 +0200

    Fragments und Activities für die einzelnen Items im Navigationdrawer funktionuckelt sogar

--- app/src/main/java/me/olisonsturm/blackout/view/LobbyActivity.java
+++ app/src/main/java/me/olisonsturm/blackout/view/LobbyActivity.java
@@ -1,5 +1,6 @@
 package me.olisonsturm.blackout.view;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -7,12 +8,21 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.google.android.material.navigation.NavigationView;
+
+import me.olisonsturm.blackout.LobbyFragment;
 import me.olisonsturm.blackout.R;
+import me.olisonsturm.blackout.SettingsFragment;
+import me.olisonsturm.blackout.SpieleFragment;
+import me.olisonsturm.blackout.StatisticsFragment;
 
-public class LobbyActivity extends AppCompatActivity {
+public class LobbyActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawer;
-            Toolbar toolbar;
+    Toolbar toolbar;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,22 +31,60 @@ public class LobbyActivity extends AppCompatActivity {
 
         drawer = findViewById(R.id.drawer_layout);
         toolbar = findViewById(R.id.myToolbar);
+        navigationView = findViewById(R.id.nav_view);
+
+        navigationView.setNavigationItemSelectedListener(this);
+
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new LobbyFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_lobby);
+        }
+
     }
 
     @Override
-    public void onBackPressed(){
-        if(drawer.isDrawerOpen(GravityCompat.START)){
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_lobby:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new LobbyFragment()).commit();
+                break;
+
+            case R.id.nav_games:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new SpieleFragment()).commit();
+                break;
+
+            case R.id.nav_statistics:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new StatisticsFragment()).commit();
+                break;
+
+            case R.id.nav_settings:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new SettingsFragment()).commit();
+                break;
+
+            case R.id.nav_appinfo:
+                Toast.makeText(this, "Daten über die App", Toast.LENGTH_SHORT).show();
+                break;
+
+
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
-        }else {
+        } else {
             super.onBackPressed();
         }
     }
-
 
 
 }
