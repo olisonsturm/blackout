81d4cdbc872580e53ed629bd8e4175618a28f42a
Niklas Decker <niki25.9@web.de>
2021-10-05 20:36:28 +0200

    joo

---
+++ app/src/main/java/me/olisonsturm/blackout/model/PlayerViewModel.java
@@ -0,0 +1,45 @@
+package me.olisonsturm.blackout.model;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public class PlayerViewModel extends AndroidViewModel {
+    private PlayerRepository repository;
+    private LiveData<List<Player>> allPlayers;
+
+
+
+    public PlayerViewModel(@NonNull @NotNull Application application) {
+        super(application);
+        repository = new PlayerRepository(application);
+        allPlayers = repository.getAllPlayer();
+    }
+
+    public void insert(Player player){
+        repository.insert(player);
+    }
+
+    public void update(Player player){
+        repository.update(player);
+    }
+
+    public void delete(Player player){
+        repository.delete(player);
+    }
+
+    public void deleteAllPlayers(){
+        repository.deleteAllPlayers();
+    }
+
+    public LiveData<List<Player>> getAllPlayers(){
+        return allPlayers;
+    }
+
+}
