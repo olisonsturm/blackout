6712828c60fa1a4a92c04dffc43897548ce17ae8
Niklas Decker <niki25.9@web.de>
2022-01-22 16:44:38 +0100

    - Spielauswahl erstellt in SpieleFragment.java und fragment_spiele.xml, soll als Vorlage für die kontreten Spiele dienen
    noch zu machen:
    - Player ändern

--- app/src/main/java/me/olisonsturm/blackout/view/fragments/SpieleFragment.java
+++ app/src/main/java/me/olisonsturm/blackout/view/fragments/SpieleFragment.java
@@ -8,10 +8,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
+import androidx.transition.AutoTransition;
+import androidx.transition.ChangeTransform;
+import androidx.transition.TransitionManager;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -21,20 +27,62 @@ import me.olisonsturm.blackout.R;
 
 public class SpieleFragment extends Fragment {
 
-    TextView textView;
-    EditText editText;
-    Button btn;
+    CardView card1;
+    CardView card2;
 
-    BluetoothAdapter bluetoothAdapter;
-    BluetoothSocket bluetoothSocket;
-    InputStream inputStream;
-    OutputStream outputStream;
+    ImageButton btnGame1;
+    ImageButton btnGame2;
+
+    TextView text1;
 
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstance) {
         View view = inflater.inflate(R.layout.fragment_spiele, container, false);
+
+
+        card1 = view.findViewById(R.id.card1);
+        card2 = view.findViewById(R.id.card2);
+
+        btnGame1 = view.findViewById(R.id.btn_game1);
+        btnGame2 = view.findViewById(R.id.btn_game2);
+
+        text1 = view.findViewById(R.id.hiddenTextGame1);
+
+
+
+        card1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Transition zu Spiel 1
+            }
+        });
+
+        btnGame1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (text1.getVisibility() == View.VISIBLE){
+                    //schließen
+                    TransitionManager.beginDelayedTransition(card1, new ChangeTransform());
+                    text1.setVisibility(View.GONE);
+                    btnGame1.setImageResource(R.drawable.ic_expand_more);
+                }
+                else{
+                    //öffnen
+                    TransitionManager.beginDelayedTransition(card1, new AutoTransition());
+                    text1.setVisibility(View.VISIBLE);
+                    btnGame1.setImageResource(R.drawable.ic_expand_less);
+                }
+            }
+        });
+
+
+
+
+
+
+
 
         return view;
     }
